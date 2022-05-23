a0696791567bc9b850ccf30dd6f105f7e6484fc8
jeff-vincent <jeff.d.vincent@gmail.com>
2022-05-22 17:25:24 -0700

    Data Source VMs WIP

--- orka/data_source_vms.go
+++ orka/data_source_vms.go
@@ -13,45 +13,21 @@ func dataSourceVMs() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: dataSourceVMsRead,
 		Schema: map[string]*schema.Schema{
-			"id": {
-				Type:     schema.TypeInt,
-				Required: true,
+			"message": {
+				Type:     schema.TypeString,
+				Computed: true,
 			},
-			"items": {
+			"help": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"errors": {
 				Type:     schema.TypeList,
 				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"coffee_id": {
-							Type:     schema.TypeList,
-							Computed: true,
-						},
-						"coffee_name": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"coffee_teaser": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"coffee_description": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"coffee_price": {
-							Type:     schema.TypeInt,
-							Computed: true,
-						},
-						"coffee_image": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"quantity": {
-							Type:     schema.TypeInt,
-							Computed: true,
-						},
-					},
-				},
+			},
+			"virtual_machine_resources": {
+				Type:     schema.TypeList,
+				Computed: true,
 			},
 		},
 	}
@@ -68,5 +44,6 @@ func dataSourceVMsRead(ctx context.Context, d *schema.ResourceData, m interface{
 		return diag.FromErr(err)
 	}
 	fmt.Println(vms)
+
 	return diags
 }
